f5e536bb7c8c23822c79f7f1c9c982828542dcd7
ayoyinka245 <kolawoleayoyinka.ka@gmail.com>
2024-01-21 08:16:31 +0000

    Edited provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,22 @@
+#provider "aws" {
+ # region = "us-east-2"
+ # access_key = "AKIA5DLBVOZXMJATY3EX"
+ # secret_key = "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"
+#}
+
+# Terraform Block
+terraform {
+  required_version = "~> 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block 
 provider "aws" {
-  region = "us-east-2"
-  access_key = "AKIA5DLBVOZXMJATY3EX"
-  secret_key = "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"
+  region  = "us-east-2"
+  profile = "default"
 }
